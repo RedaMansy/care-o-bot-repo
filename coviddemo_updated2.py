491620978508ef1609e6e58f98c3d664c9f36541
RedaMansy <mansyr@cardiff.ac.uk>
2020-11-27 11:15:30 +0100

    made changes to sync motion by removing a loop and adding sleep times

--- coviddemo_updated2.py
+++ coviddemo_updated2.py
@@ -25,15 +25,15 @@ if __name__ == '__main__':
 	# start demo
 	sss.say("sound",["start"])
 	rospy.sleep(2)
-	
-	sss.say("sound", ["Hello my name is Care-O-Bot from the Human robot interaction lab, Centre for Artificial Intelligence, Robotics and Human-Machine Systems, I roms, at Cardiff University. On behalf of I rohms, I am going to guide you through some of the rules you need to follow if you are visiting campus during these unprecedented times."], False) 
+	sss.say("sound", ["Hello my name is Care-O-Bot from I roms at Cardiff University. If you are visiting campus during this unprecedented time, please follow these regulations."], False) 
 	sss.set_mimic("mimic", "laughing")
 	sss.set_mimic("mimic", "laughing")
 	handle_arm = sss.move("arm_right","hello",False)
-	sss.move("torso", [[0,0.75]])
-	sss.move("torso", [[0,-0.75]])
+	sss.move("torso", [[0,0.45]])
+	sss.move("torso", [[0,-0.45]])
 	sss.move("torso", "home")
-	handle_arm = sss.move("arm_right", "hello")
+	# handle_arm = sss.move("arm_right", "hello")
+	rospy.sleep(3)
 
 	"""
 	===========================
@@ -44,9 +44,10 @@ if __name__ == '__main__':
 	"""
 
 
-	sss.say("sound",["Firstly, make sure to wear a face covering when inside University buildings"], False)
+	#sss.say("sound",["Firstly, make sure to wear a face covering when inside University buildings"], False)
 	handle_arm = sss.move("arm_right", [[0,-1,0.9,-2.1,1,0,0]], False)
 	handle_arm = sss.move("arm_left", [[0,1,-0.9,2.1,-1,0,0]])
+	sss.say("sound",["Firstly, make sure to wear a face covering when inside University buildings"], False)
 
 	sss.move("arm_right", "side", False)
 	sss.move("arm_left", "side")
@@ -62,31 +63,40 @@ if __name__ == '__main__':
 	"""
 
 
-	sss.say("sound", ["Adhere to social distancing guidelines and stay two metres away from others where possible."], False)
+	# sss.say("sound", ["Adhere to social distancing guidelines and stay two metres away from others where possible."], False)
 	handle_arm = sss.move("arm_right", "home", False)
 	handle_arm = sss.move("arm_left", "home")
-	sss.move("torso", [[0,0.3]])
-	sss.move("torso", [[0,-0.3]])
+	sss.say("sound", ["Adhere to social distancing guidelines and stay two metres away from others where possible."], False)
+	sss.move("torso", [[0,0.4]])
+	sss.move("torso", [[0,-0.4]])
 	sss.move("torso", "home")
-	sss.say("sound", ["You must also follow the signs around campus, including following the one way system."], False)
+	# sss.say("sound", ["You must also follow the signs around campus, including following the one way system."], False)
 	# handle_arm = sss.move("arm_right", [[2.4, 1.4, -1.0472, 0, -1.0472, -0.6981, 1.0700]], False)
 	# handle_arm = sss.move("arm_left", [[-2.4, -1.4, 1.0472, 0, 1.0472, 0.6981, -1.0700]])
-	for i in range(3):
-		handle_arm = sss.move("arm_right", [[1.4, 1.3, -1.0472, -1.9, -1.0472, -0.6981, 1.0700]], False)
-		handle_arm = sss.move("arm_left", [[-1.4, -1.3, 1.0472, 1.9, 1.0472, 0.6981, -1.0700]])
-		handle_arm = sss.move("arm_right", [[1.4, 1.3, -1.0472, -1.5, -1.0472, -0.6981, 1.0700]], False)
-		handle_arm = sss.move("arm_left", [[-1.4, -1.3, 1.0472, 1.5, 1.0472, 0.6981, -1.0700]])
+	
+	handle_arm = sss.move("arm_right", [[1.4, 1.3, -1.0472, -1.9, -1.0472, -0.6981, 1.0700]], False)
+	handle_arm = sss.move("arm_left", [[-1.4, -1.3, 1.0472, 1.9, 1.0472, 0.6981, -1.0700]])
+	sss.say("sound", ["You must also follow the signs around campus, including following the one way system."], False)
+
+	handle_arm = sss.move("arm_right", [[1.4, 1.3, -1.0472, -1.5, -1.0472, -0.6981, 1.0700]], False)
+	handle_arm = sss.move("arm_left", [[-1.4, -1.3, 1.0472, 1.5, 1.0472, 0.6981, -1.0700]])
+
+	handle_arm = sss.move("arm_right", [[1.4, 1.3, -1.0472, -1.9, -1.0472, -0.6981, 1.0700]], False)
+        handle_arm = sss.move("arm_left", [[-1.4, -1.3, 1.0472, 1.9, 1.0472, 0.6981, -1.0700]])
+        handle_arm = sss.move("arm_right", [[1.4, 1.3, -1.0472, -1.5, -1.0472, -0.6981, 1.0700]], False)
+        handle_arm = sss.move("arm_left", [[-1.4, -1.3, 1.0472, 1.5, 1.0472, 0.6981, -1.0700]])
 
 	"""
 	==================================
 	Hand Washing
 	TODO: Rotate one of the grippers
 	==================================
-
 	"""
+	
+	rospy.sleep(1)
 	sss.say("sound",["Remember to sanitise your hands regularly using the sanitisers available around the buildings and on arrival at the campus, as well as regular 20 second hand washing"], False)
 
-	for i in range(3):
+	for i in range(2):
  		handle_arm = sss.move("arm_left", [[1.02, 1.5, -1.4, 1.9, -1.84, -1.12, -0.64]], False)
  		handle_arm = sss.move("arm_right", [[-1.25, -1.5, 1.4, -1.9, 1.84, 1.12, 0.64]])
  		handle_arm = sss.move("arm_left", [[1.02, 1.3, -1.4, 1.9, -1.84, -1.12, -0.64]], False)
@@ -103,19 +113,8 @@ if __name__ == '__main__':
 	==========
 
 	"""	
-
-	handle_arm = sss.move("arm_right", "folded", False)
-	handle_arm = sss.move("arm_left", "folded")
-
-	sss.say("sound",["Thank you for your co operation"], False)
-	
-	sss.move("torso", [[-0.3,0.2]])
-	sss.move("torso", "front")
-
-
-	handle_arm = sss.move("arm_right", "side", False)
-	handle_arm = sss.move("arm_left", "side")
-	sss.say("sound",["Stay Safe"], False)
+	sss.say("sound",["Thank you for your co operation and stay safe"], False)
+	sss.set_mimic("mimic", "laughing")
 
 	
 
